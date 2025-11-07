1e5362bb67dcbca9533d68184dde044ca0d507c8
coraliney <carro_gryglas@hotmail.com>
2025-11-07 06:33:57 +0100

    :construction: delivering a structure

---
+++ my-react-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Navbar />
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+</script>
+
+<style>
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #111;
+}</style>
